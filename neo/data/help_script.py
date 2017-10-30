9afca6cf8584cd65f0f43258da52ee4238086e59
Sinan Ulkuatam <sulkuatam@gmail.com>
2017-10-30 11:22:31 -0400

    cleaned data with results on neo

--- neo/data/help_script.py
+++ neo/data/help_script.py
@@ -1,10 +1,11 @@
 import csv
-
+import pandas as pd
 # note: small bug causing most recent date row to not work
 
 out = open("neo_price_new.csv", 'w')
 csv_header = "date,open,high,low,close,volume,market_cap,increase_flag,percent_change\n"
 out.write(csv_header)
+output=[]
 
 with open('neo_price.csv', 'rU') as csvfile:
 	neoreader = csv.reader(csvfile, delimiter=',')
@@ -12,6 +13,11 @@ with open('neo_price.csv', 'rU') as csvfile:
 	for i, row in enumerate(neoreader):
 		row[5]=row[5].replace(',', '')
 		row[6]=row[6].replace(',', '')
+
+		if row[4] == '':
+			row[4]=1
+		if row[1] == '':
+			row[1]=1
 
 		x = (float(row[4])-float(row[1]))/(float(row[1])) * 100
 
@@ -22,7 +28,6 @@ with open('neo_price.csv', 'rU') as csvfile:
 
 		row.append(x)
 
-		print row
 		with open("neo_price_new.csv", "a") as fp:
 		    wr = csv.writer(fp, dialect='excel')
 		    wr.writerow(row)		
